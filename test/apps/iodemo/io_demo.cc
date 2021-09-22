ded1983ae9b2fb73d012b871f16ecc4400b29903
Sergey Oblomov <sergeyo@nvidia.com>
2021-09-22 11:10:30 +0300

    UCP/TAG/SEND: added eager/rndv flags to tag_send op
    - added op-flags support to tag_send_[sync]_nbx functions
    - added flags eager/rndv flags to force used protocol

--- test/apps/iodemo/io_demo.cc
+++ test/apps/iodemo/io_demo.cc
@@ -87,6 +87,7 @@ typedef struct {
     bool                     use_epoll;
     ucs_memory_type_t        memory_type;
     unsigned                 progress_count;
+    size_t                   rndv_thresh;
 } options_t;
 
 #define LOG_PREFIX  "[DEMO]"
@@ -766,7 +767,8 @@ protected:
 
     P2pDemoCommon(const options_t &test_opts) :
         UcxContext(test_opts.iomsg_size, test_opts.connect_timeout,
-                   test_opts.use_am, test_opts.use_epoll),
+                   test_opts.use_am, test_opts.rndv_thresh,
+                   test_opts.use_epoll),
         _test_opts(test_opts),
         _io_msg_pool(test_opts.iomsg_size, "io messages"),
         _send_callback_pool(0, "send callbacks"),
@@ -968,7 +970,7 @@ public:
                 if (_server->opts().validate) {
                     validate(_conn, *_iov, _sn, IO_WRITE);
                 }
-                
+
                 if (_conn->ucx_status() == UCS_OK) {
                     _server->send_io_write_response(_conn, *_iov, _sn);
                 }
@@ -2491,9 +2493,10 @@ static int parse_args(int argc, char **argv, options_t *test_opts)
     test_opts->use_epoll             = false;
     test_opts->memory_type           = UCS_MEMORY_TYPE_HOST;
     test_opts->progress_count        = 1;
+    test_opts->rndv_thresh           = UcxContext::rndv_thresh_auto;
 
     while ((c = getopt(argc, argv,
-                       "p:c:r:d:b:i:w:a:k:o:t:n:l:s:y:vqeADHP:m:L:")) != -1) {
+                       "p:c:r:d:b:i:w:a:k:o:t:n:l:s:y:vqeADHP:m:L:R:")) != -1) {
         switch (c) {
         case 'p':
             test_opts->port_num = atoi(optarg);
@@ -2638,6 +2641,9 @@ static int parse_args(int argc, char **argv, options_t *test_opts)
                 return -1;
             }
             break;
+        case 'R':
+            test_opts->rndv_thresh = strtol(optarg, NULL, 0);
+            break;
         case 'h':
         default:
             std::cout << "Usage: io_demo [options] [server_address]" << std::endl;
@@ -2671,6 +2677,10 @@ static int parse_args(int argc, char **argv, options_t *test_opts)
             std::cout << "  -D                          Enable debugging mode for IO operation timeouts" << std::endl;
             std::cout << "  -H                          Use human-readable timestamps" << std::endl;
             std::cout << "  -P <interval>               Set report printing interval"  << std::endl;
+            std::cout << "  -R <threshold>              Always use rendezvous protocol for messages starting" << std::endl;
+            std::cout << "                              from this size, and eager protocol for" << std::endl;
+            std::cout << "                              messages lower than this size. If not set," << std::endl;
+            std::cout << "                              the threshold is selected automatically by UCX" << std::endl;
             std::cout << "" << std::endl;
             std::cout << "  -m <memory_type>            Memory type to use. Possible values: host"
 #ifdef HAVE_CUDA
