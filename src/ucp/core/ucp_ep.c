27f2cb0a950a656744bedb22648fb4481a8e9fec
Evgeny Leksikov <evgenylek@mellanox.com>
2025-04-28 19:35:55 +0300

    UCP/CONFIG: Enhance endpoint configuration comparison by introducing flags for lane-only checks and improving equality logic

--- src/ucp/core/ucp_ep.c
+++ src/ucp/core/ucp_ep.c
@@ -1955,7 +1955,7 @@ int ucp_ep_config_lane_is_equal(const ucp_ep_config_key_t *key1,
 int ucp_ep_config_is_equal(const ucp_ep_config_key_t *key1,
                            const ucp_ep_config_key_t *key2)
 {
-    return ucp_ep_config_is_equal2(key1, key2, 0);
+    return ucp_ep_config_is_equal2(key1, key2, UCP_EP_CONFIG_CMP_FLAG_ALL);
 }
 
 int ucp_ep_config_is_equal2(const ucp_ep_config_key_t *key1,
@@ -1964,6 +1964,8 @@ int ucp_ep_config_is_equal2(const ucp_ep_config_key_t *key1,
     ucp_lane_index_t lane;
     int i;
 
+    ucs_assert(flags & UCP_EP_CONFIG_CMP_FLAG_LANES);
+
     if ((key1->num_lanes != key2->num_lanes) ||
         memcmp(key1->rma_lanes, key2->rma_lanes, sizeof(key1->rma_lanes)) ||
         memcmp(key1->am_bw_lanes, key2->am_bw_lanes,
@@ -1971,18 +1973,12 @@ int ucp_ep_config_is_equal2(const ucp_ep_config_key_t *key1,
         memcmp(key1->rma_bw_lanes, key2->rma_bw_lanes,
                sizeof(key1->rma_bw_lanes)) ||
         memcmp(key1->amo_lanes, key2->amo_lanes, sizeof(key1->amo_lanes)) ||
-        (key1->rma_bw_md_map != key2->rma_bw_md_map) ||
-        (key1->rma_md_map != key2->rma_md_map) ||
-        (key1->reachable_md_map != key2->reachable_md_map) ||
         (key1->am_lane != key2->am_lane) ||
         (key1->tag_lane != key2->tag_lane) ||
         (key1->wireup_msg_lane != key2->wireup_msg_lane) ||
         (key1->cm_lane != key2->cm_lane) ||
         (key1->keepalive_lane != key2->keepalive_lane) ||
-        (key1->rkey_ptr_lane != key2->rkey_ptr_lane) ||
-        (key1->err_mode != key2->err_mode) ||
-        (key1->flags != key2->flags) ||
-        (key1->dst_version != key2->dst_version)) {
+        (key1->rkey_ptr_lane != key2->rkey_ptr_lane)) {
         return 0;
     }
 
@@ -1990,6 +1986,20 @@ int ucp_ep_config_is_equal2(const ucp_ep_config_key_t *key1,
         if (!ucp_ep_config_lane_is_equal(key1, key2, lane)) {
             return 0;
         }
+    }
+
+    /* If we are comparing lanes only, we are done */
+    if (flags != UCP_EP_CONFIG_CMP_FLAG_ALL) {
+        return 1;
+    }
+
+    if ((key1->rma_bw_md_map != key2->rma_bw_md_map) ||
+        (key1->rma_md_map != key2->rma_md_map) ||
+        (key1->reachable_md_map != key2->reachable_md_map) ||
+        (key1->err_mode != key2->err_mode) ||
+        (key1->flags != key2->flags) ||
+        (key1->dst_version != key2->dst_version)) {
+        return 0;
     }
 
     for (i = 0; i < ucs_popcount(key1->reachable_md_map); ++i) {
